4c1b9b81c98ea34fbb2feb43cef540e97daf0150
Андрей Козлов <61379977+yellowpearl@users.noreply.github.com>
2026-08-18 16:30:18 +0300

    Time: 1040 ms (68.26%), Space: 19.5 MB (32.32%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -2,6 +2,8 @@ import heapq
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         # Буду решать через дп, справа налево буду считать сколько значений справа больше текущего числа, буду идти до первого O(n**2) O(n)
+
+        # Фикс - нужно идти до максимального, а не до первого
         res = 0
         dp = [0] * len(nums)
         for i in range(len(nums)-1, -1, -1):
